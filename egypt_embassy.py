d9971cbb8f74278f72c4f9021982813d638a9868
MohammedEsamaldin <mohammedesam.mem@gmail.com>
2023-11-19 19:20:47 +0200

    second commit

---
+++ egypt_embassy.py
@@ -0,0 +1,74 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# Initialize the SMTP server for sending email notifications
+def send_email_notification(subject, message):
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+    smtp_username = "mohammedesam.mem@gmail.com"
+    smtp_password = "bpvo xmja fijr yfkg"
+    sender_email = "mohammedesam.mem@gmail.com"
+    receiver_email = "mohammedesam.mem@gmail.com"
+
+    msg = MIMEMultipart()
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    msg["Subject"] = subject
+
+    body = message
+    msg.attach(MIMEText(body, "plain"))
+
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+        text = msg.as_string()
+        server.sendmail(sender_email, receiver_email, text)
+        server.quit()
+        print("Email notification sent successfully.")
+    except Exception as e:
+        print(f"Failed to send email notification: {e}")
+
+def check_availability():
+    chrome_options = Options()
+    # You can add more options as required
+    chrome_options.add_argument("--headless")  # Runs Chrome in headless mode.
+    chrome_options.add_argument('start-maximized') 
+    chrome_options.add_argument('disable-infobars')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome()
+    driver.get("https://egyptianconsulateindubai.as.me/schedule.php")
+
+    try:
+        time.sleep(5)  # Wait for the page to load
+        message_element = driver.find_element(By.XPATH, '//*[contains(text(), "No times are available")]')
+        if message_element:
+            print("No new available times.")
+            send_email_notification("New Availability Alert","No new available times.")
+        else:
+            send_email_notification("New Availability Alert", "New time slots might be available on the website.")
+    except NoSuchElementException:
+        send_email_notification("New Availability Alert", "New time slots might be available on the website.")
+    finally:
+        driver.quit()
+
+# Continuously check every 10 minutes
+while True:
+    check_availability()
+    print("Waiting 10 minutes before the next check...")
+
+    time.sleep(60)  # Wait for 10 minutes (600 seconds) before checking again
+
+
+
+
